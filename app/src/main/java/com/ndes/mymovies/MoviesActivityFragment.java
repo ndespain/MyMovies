c3a55724e85c7aa6e87d886c01740a40d5633229
ndespain <ndespain@ancestry.com>
2015-09-04 16:31:31 -0600

    Adding request for movies.

--- app/src/main/java/com/ndes/mymovies/MoviesActivityFragment.java
+++ app/src/main/java/com/ndes/mymovies/MoviesActivityFragment.java
@@ -1,7 +1,10 @@
 package com.ndes.mymovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,8 @@ import android.widget.GridView;
 
 import com.squareup.picasso.Picasso;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,12 +46,65 @@ public class MoviesActivityFragment extends Fragment {
         //http://api.themoviedb.org/3/discover/movie?page=1&release_date.gte=2013-05-12&release_date.lte=2013-05-14&api_key=522f2ef5fc7f003a64790b1c6016ec3a
 //        http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg
 //        Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        //http://api.themoviedb.org/3/discover/movie?api_key=522f2ef5fc7f003a64790b1c6016ec3a&certification_country=US&certification.lte=PG-13
 
         mMovieAdapter = new MovieArrayAdapter(getActivity(), R.layout.grid_item_movie, movies);
         GridView moviesGrid = (GridView) view.findViewById(R.id.gridViewMovies);
         moviesGrid.setAdapter(mMovieAdapter);
 
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovies();
+    }
+
+    private void updateMovies() {
+
+        new MoviesTask().execute();
+
+    }
+
+    public class MoviesTask extends AsyncTask <Void, Void, MovieData[]> {
+
+        private final String LOG_TAG = MoviesTask.class.getSimpleName();
+        private final String BASE_URL = "http://api.themoviedb.org/3/discover/movie?";
+        private final String API_KEY = "522f2ef5fc7f003a64790b1c6016ec3a";
+
+        @Override
+        protected MovieData[] doInBackground(Void... params) {
+            String fullData = getMovieData();
+            MovieData[] movies = null;
+
+            movies = getMovieDataFromJson(fullData);
+
+            return movies;
+        }
+
+        private MovieData[] getMovieDataFromJson(String fullData) {
+            return new MovieData[0];
+        }
+
+        private String getMovieData() {
+
+
+            Uri uri = Uri.parse(BASE_URL).buildUpon().appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("certification_country", "US")
+                    .appendQueryParameter("certification.lte","PG-13").build();
+
+
+            URL url = null;
+            try {
+                url = new URL(uri.toString());
+            } catch (MalformedURLException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                e.printStackTrace();
+            }
+            return ServiceUtils.requestData(url);
+
+        }
     }
 }
 
