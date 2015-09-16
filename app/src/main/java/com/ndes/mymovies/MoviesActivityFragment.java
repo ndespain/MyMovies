8c622258c6ed3eb3d630e899ca46ed774226b0e2
ndespain <ndespain@ancestry.com>
2015-09-16 14:35:06 -0600

    layout stuff

--- app/src/main/java/com/ndes/mymovies/MoviesActivityFragment.java
+++ app/src/main/java/com/ndes/mymovies/MoviesActivityFragment.java
@@ -147,6 +147,7 @@ public class MoviesActivityFragment extends Fragment {
 
 /*
 http://docs.themoviedb.apiary.io/#reference/discover/discovermovie
+https://www.themoviedb.org/documentation/api/discover
 sort_by
     popularity.asc
     popularity.desc
@@ -186,40 +187,3 @@ sort_by
 }
 
 
-/*
-// show The Image
-new DownloadImageTask((ImageView) findViewById(R.id.imageView1))
-            .execute("http://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png");
-}
-
-public void onClick(View v) {
-    startActivity(new Intent(this, IndexActivity.class));
-    finish();
-
-}
-
-private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-    ImageView bmImage;
-
-    public DownloadImageTask(ImageView bmImage) {
-        this.bmImage = bmImage;
-    }
-
-    protected Bitmap doInBackground(String... urls) {
-        String urldisplay = urls[0];
-        Bitmap mIcon11 = null;
-        try {
-            InputStream in = new java.net.URL(urldisplay).openStream();
-            mIcon11 = BitmapFactory.decodeStream(in);
-        } catch (Exception e) {
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
-        }
-        return mIcon11;
-    }
-
-    protected void onPostExecute(Bitmap result) {
-        bmImage.setImageBitmap(result);
-    }
-}
- */
